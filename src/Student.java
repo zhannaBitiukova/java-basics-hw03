8b2189e8420e9817f14a88204c3e3c0afaaffa89
Zhanna Bitiukova <zhanna.bitiukova@gmail.com>
2022-05-24 23:49:41 +0300

    Ready

--- src/Student.java
+++ src/Student.java
@@ -5,130 +5,133 @@ public class Student {
   public static int totalRating = 0;
   public static double avgRating = 0.0d;
 
-  // implement Student class according to the instructions provided in the README.md file
   public Student() {
+    // create an empty Student entity
     count++;
   }
 
   public Student(String name) {
-    this.name = name;
+    // create a student with specified name
     count++;
+    this.name = name;
   }
 
   public Student(String name, int rating) {
+    // create a student with specified name and rating
+    count++;
     this.name = name;
     this.rating = rating;
-    count++;
     totalRating += rating;
-  }
-
-  public static double getAvgRating() {
-    // return average rating of all students
-    if (count == 0) {
-      avgRating = 0.0d;
-    }
-    else {
-      avgRating = (double) totalRating / (double)  count;
-    }
-    return avgRating;
+    calculateAvgRating();
   }
 
   public String getName() {
+    // get student name
     return name;
   }
 
   public void setName(String name) {
+    // set student name
     this.name = name;
   }
 
   public int getRating() {
+    // get student rating
     return this.rating;
   }
 
   public void setRating(int rating) {
-    // when a rating is set to a student for the first time
-    // we need to add the rating to totalRating
-    // This method MUST NOT be used to change rating second and further times
+    // initialise (set for the first time) student's rating
+    // DO NOT USE this method to update an existent student rating
     this.rating = rating;
     totalRating += rating;
+    calculateAvgRating();
   }
 
-  public boolean betterStudent(Student student) {
-    boolean isBetterStudent = false;
+  public static double getAvgRating() {
+    // return average rating of all students
+    return avgRating;
+  }
 
-    if (this.rating > student.rating) {
-      isBetterStudent = true;
+  public static void calculateAvgRating() {
+    // calculate average rating of all students
+    if (count == 0) {
+      avgRating = 0.0d;
     }
-
-    return isBetterStudent;
+    else {
+      avgRating = (double) totalRating / (double) count;
+    }
   }
 
   public void changeRating(int rating) {
-    // TODO change this student's rating and average rating of all students
-    // when a new rating is set to a student NOT for the first time
-    // we need to remove their old rating from totalRating
-    //and then add the new rating to totalRating
+    // update current student rating
+    // DO NOT use this method to initialize student rating
+    // Use this method every time when need to update existing rating
     totalRating -= this.rating;
     this.rating = rating;
     totalRating += rating;
+    calculateAvgRating();
+  }
+
+  public boolean betterStudent(Student student) {
+    // compare if the current student is better than another selected one
+    return this.rating > student.rating;
   }
 
   public static void removeStudent(Student student) {
-    // when removing student we must update the static fields:
-    //1. decrement count of students
-    //2. take student's rating out of the total rating
-    //3. re-calcuate average rating using the new count and totalRating
+    // remove student
     count--;
     totalRating -= student.rating;
-    getAvgRating();
+    calculateAvgRating();
+  }
+
+  public static void printCommonStats() {
+    // print data related to all students
+    String roundedAvgRating = String.format("%.02f", Student.getAvgRating());
+
+    System.out.println("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+    System.out.println("Student count:  " + Student.count);
+    System.out.println("Total rating:   " + Student.totalRating);
+    System.out.println("Average rating: " + roundedAvgRating);
+  }
+
+  public static void printPersonalData(Student... student) {
+    // print data related to all students
+    System.out.println("----------------------------------------------------------------");
+    for (Student person : student)
+      System.out.println(person.toString());
   }
 
   @Override
   public String toString() {
-    // return String with name and rating of this student
-    String studentInfoString = this.name + ", rating " + this.rating;
-    return studentInfoString;
+    // return String with name and rating of the current student
+    return "               - " + this.name + ", rating " + this.rating;
   }
 
   public static void main(String[] args) {
-/*
-    double averageRating = Student.getAvgRating();
+    // Display initial data
+    printCommonStats();
 
-    System.out.println(Student.count);
-    System.out.println(Student.totalRating);
-    System.out.println(Student.avgRating);
-    System.out.println(averageRating);
-    */
+    // Create 3 objects of Student type and input information about them
+    Student student1 = new Student();
+    student1.setName("Marichka");
+    student1.setRating(50);
 
+    Student student2 = new Student("Olena");
+    student2.setRating(16);
 
-    //Student student1 = new Student("Marichka",15);
-    //Student student2 = new Student("Olena", 30);
-    //Student student3 = new Student("Bohdan", 13);
-/*
-    averageRating = Student.getAvgRating();
-    System.out.println(averageRating);
+    Student student3 = new Student("Bohdan", 68);
 
-    student1.setRating(45);
-    averageRating = Student.getAvgRating();
-    System.out.println(averageRating);
-*/
+    // Display the average rating of all students
+    printCommonStats();
+    printPersonalData(student1, student2, student3);
 
-    double averageRating = Student.getAvgRating();
-    System.out.println(averageRating);
+    // Change the rating of a student.
+    student1.changeRating(4);
 
-    /*
-    Student student1 = new Student("Petro");
-    student1.setRating(15);
-    Student student2 = new Student("Volodymyr");
-    student2.setRating(30);
-     */
-    Student student1 = new Student("Marichka",15);
-    Student student2 = new Student("Olena", 30);
-    Student student3 = new Student("Bohdan", 13);
-
-    averageRating = Student.getAvgRating();
-    System.out.println("Average student rating is " + averageRating);
-
+    // Display the new average rating.
+    printCommonStats();
+    printPersonalData(student1, student2, student3);
   }
 }
 
